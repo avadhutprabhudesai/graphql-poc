da540a85be0d3278e075a2135d4af9d1803b0094
Avadhut Prabhudesai <avadhutprabhudesai23@gmail.com>
2022-06-22 10:17:35 +0530

    Important features of Apollo Server are demonstrated.

---
+++ src/data/employees.js
@@ -0,0 +1,20 @@
+export const employees = [
+  {
+    id: 1,
+    name: 'John Doe',
+    department: 'ENG',
+    programmingLanguages: ['Node', 'React'],
+  },
+  {
+    id: 2,
+    name: 'Bob Smith',
+    department: 'MGR',
+    projects: ['Cargo', 'NASA'],
+  },
+  {
+    id: 3,
+    name: 'Jane Doe',
+    department: 'ACC',
+    financialTools: ['SAP', 'Tally'],
+  },
+];
